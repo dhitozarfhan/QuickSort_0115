a531fa5c7513b1ec5d9580945200ae0bdf8ca1f6
dhitozarfhan <dhitozarfhan@gmail.com>
2024-03-29 10:29:07 +0700

    variaable cmpt_count

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -18,4 +18,17 @@ void input() {
 
 	}
 
+	cout << "\n-----------------" << endl;
+	cout << "\nenter arry element" << endl;
+	cout << "\n-----------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+
+}
+
+
 

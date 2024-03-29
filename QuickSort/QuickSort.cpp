f26beb08f549dd49677cddc8ef874d79fc2a8ca6
dhitozarfhan <dhitozarfhan@gmail.com>
2024-03-29 10:40:10 +0700

    void display

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -78,6 +78,20 @@ void q_short(int low, int high)
 	q_short(j + 1, high);
 }
 
+void display()
+{
+	cout << "\n-------------" << endl;
+	cout << "sorted arrray" << endl;
+	cout << "-------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << "\n\nNumber of comparasions : " << cmp_count << endl;
+	cout << "number of data movement : " << mov_count << endl;
+}
+
 
 
 
